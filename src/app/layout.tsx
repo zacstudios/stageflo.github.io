c3e44d216f03617b6b54baaa695ce0f87bbe7d91
Prince <prince@zacstudios.com>
2026-05-24 22:47:41 +0100

    chore: update site icon and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,14 @@ export const metadata: Metadata = {
   alternates: {
     canonical: "/",
   },
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/icon.png", type: "image/png" },
+    ],
+    apple: [{ url: "/apple-icon.png", sizes: "180x180", type: "image/png" }],
+    shortcut: ["/favicon.ico"],
+  },
   openGraph: {
     type: "website",
     siteName: "StageFlo",
